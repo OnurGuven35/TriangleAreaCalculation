15c9ecc22d3964fa78d330cf6eda46bbeb93bbbc
OnurGuven35 <gvnn.onrr@gmail.com>
2022-05-19 23:14:27 +0300

    Initial commit

---
+++ src/Area.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+
+        int a, b, c, u, du;
+        double alan;
+
+        Scanner circum = new Scanner(System.in);
+
+        System.out.print("Birinci Kenari Giriniz : ");
+        a = circum.nextInt();
+        System.out.print("Ikinci Kenari Giriniz : ");
+        b = circum.nextInt();
+        System.out.print("Ucuncu Kenari Giriniz : ");
+        c = circum.nextInt();
+
+        u = (a + b + c) / 2;
+        alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
+
+        System.out.print("Kenarlari Verilen Ucgenin Alani : " + alan);
+
+    }
+}
