867fd0c592a24b9e41c6e22557c8d3d9ac50bab0
OnurGuven35 <gvnn.onrr@gmail.com>
2022-06-13 00:20:39 +0300

    Devam

--- src/Area.java
+++ src/Area.java
@@ -1,24 +1,31 @@
+// Üç kenar uzunluğunu kullanıcıdan aldığınız üçgenin alanını hesaplayan programı yazınız.
+
 import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
+        Scanner circum = new Scanner(System.in);
 
         int a, b, c, u;
         double alan;
 
-        Scanner circum = new Scanner(System.in);
+        // İlk olarak kullanıcıdan üçgenin kenarlarıını alıyoruz.
 
-        System.out.print("Birinci Kenari Giriniz : ");
+        System.out.println();
+        System.out.print("1. Kenari Giriniz : ");
         a = circum.nextInt();
-        System.out.print("Ikinci Kenari Giriniz : ");
+        System.out.print("2. Kenari Giriniz : ");
         b = circum.nextInt();
-        System.out.print("Ucuncu Kenari Giriniz : ");
+        System.out.print("3. Kenari Giriniz : ");
         c = circum.nextInt();
+
+        // Daha sonra üçgen alan formüllerini yazarak alanı ekrana yazdırıyoruz.
 
         u = (a + b + c) / 2;
         alan = Math.sqrt(u * (u - a) * (u - b) * (u - c));
 
-        System.out.print("Kenarlari Verilen Ucgenin Alani : " + alan);
+        System.out.println();
+        System.out.println("Kenarlari Verilen Ucgenin Alani : " + alan);
 
     }
 }
